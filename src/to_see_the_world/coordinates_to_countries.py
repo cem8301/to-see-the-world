4120aabf3c2af5ea918aaa6aa11e1176e9c26c58
cem8301 <carolynmason3@gmail.com>
2025-07-25 18:00:57 -0800

    fix spacing forusing on pydroid and comment out shapely code. it wasnt fully working and doesnt work on my phone

--- src/to_see_the_world/coordinates_to_countries.py
+++ src/to_see_the_world/coordinates_to_countries.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 import pandas as pd
 from scipy.spatial import KDTree
-from shapely import STRtree
-from shapely.geometry import Point, Polygon
+#from shapely import STRtree
+#from shapely.geometry import Point, Polygon
 
 from update_local_data2 import Datasets
 
@@ -38,46 +38,61 @@ class CoordinatesToCountries:
             f'{self.pwd}/{f}', na_filter = False)
         
     def get_geodata(self, coords):
-        start_ids = coords['id']
-        df1 = self.get_geodata_strtree(coords)
-        complete_ids = list(df1.id.values)
-        missed_ids = [start_ids.index(x) for x in start_ids
-            if x not in complete_ids]
-        if len(missed_ids) == 0:
-            return df1
-        print(f'num missed_ids: {len(missed_ids)}, '
-            'running kdtree')
-        m_ids = [coords['id'][idx] for idx in missed_ids]
-        m_coords = [coords['coords'][idx] for idx in missed_ids]
-        missed_coords = {'id': m_ids, 'coords': m_coords}
-        df2 = self.get_geodata_kdtree(missed_coords)
-        df_combined = pd.concat([df1, df2], ignore_index=True)
-        return df_combined
+       # start_ids = coords['id']
+#        df1 = self.get_geodata_strtree(coords)
+#        complete_ids = list(df1.id.values)
+#        missed_ids = [start_ids.index(x) for x in
+#            start_ids if x not in complete_ids]
+#        if len(missed_ids) == 0:
+#            return df1
+#        print(f'num missed_ids: {len(missed_ids)}, '
+#            'running kdtree')
+#        m_ids = [coords['id'][idx] for idx in
+#            missed_ids]
+#        m_coords = [coords['coords'][idx] for idx in
+#            missed_ids]
+#        missed_coords = {'id': m_ids,
+#            'coords': m_coords}
+#        df2 = self.get_geodata_kdtree(
+#            missed_coords)
+#        df_combined = pd.concat([df1, df2],
+#            ignore_index=True)
+#        return df_combined
+        return self.get_geodata_kdtree(coords)
     
     def clockwise_sort_polygon(self, coords):
-        centroids = self.Datasets.get_centroid(coords)
+        centroids = self.Datasets.get_centroid(
+            coords)
         center_x = centroids[1]
         center_y = centroids[0]
         def angle_to_center(coord):
-            return (math.atan2(coord[1] - center_y, coord[0
-                ] - center_x) + 2 * math.pi) % (2 * math.pi)
-        ans = sorted(coords, key=angle_to_center, reverse=True)
+            return (math.atan2(
+                coord[1] - center_y, coord[0
+                ] - center_x) + 2 * math.pi) % (
+                2 * math.pi)
+        ans = sorted(coords, 
+            key=angle_to_center, reverse=True)
         polygon = Polygon(ans)
         if not polygon.is_valid:
             print('bad', centroids)
         return polygon
 
     def get_polygons(self):
-        df_grouped = self.df_cb_shifted.groupby('fid').apply(
+        df_grouped = self.df_cb_shifted.groupby(
+            'fid').apply(
             lambda x: list(zip(x['lat'], x['lon']))
             ).reset_index(name='tuples')
-        df_grouped['tup_len'] = df_grouped.tuples.apply(len)
-        df_grouped = df_grouped.get(df_grouped.tup_len >= 4
+        df_grouped['tup_len'] = \
+            df_grouped.tuples.apply(len)
+        df_grouped = df_grouped.get(
+            df_grouped.tup_len >= 4
             ).reset_index()
-        a = self.df_cb_shifted.set_index('fid')['country_code'
+        a = self.df_cb_shifted.set_index(
+            'fid')['country_code'
             ].to_dict()
         idx_to_fid = df_grouped.to_dict()['fid']
-        cc_index_dict = {k:a[v] for k,v in idx_to_fid.items()}
+        cc_index_dict = {k:a[v] for k,v in
+            idx_to_fid.items()}
         polygons = [self.clockwise_sort_polygon(x
             ) for x in list(df_grouped.tuples.values)]
         return polygons, cc_index_dict, idx_to_fid
@@ -86,22 +101,26 @@ class CoordinatesToCountries:
         # figure out how to translate the location to the cc
         # have code choose between these two answers 305 762
         # for the spain island in France
-        polygons, cc_index_dict, idx_to_fid = self.get_polygons() 
+        polygons, cc_index_dict, idx_to_fid = \
+            self.get_polygons() 
         points = [Point(v) for v in coords['coords']]
         tree = STRtree(points)
         cgi = tree.query(polygons, predicate='contains')
         geo_data = {
-            'id': [], 'fid': [], 'country_code': [], 'og_coord': []}
+            'id': [], 'fid': [], 'country_code': [],
+            'og_coord': []}
         coords_dict = {idx:{
             'id': coords['id'][idx],
             'tuple': coords['coords'][idx]
             } for idx,_ in enumerate(coords['id'])} 
         c = dict(zip(cgi[1], cgi[0]))
         for k,v in c.items():
-            geo_data['country_code'].append(cc_index_dict[v])
+            geo_data['country_code'].append(
+                cc_index_dict[v])
             geo_data['id'].append(coords_dict[k]['id'])
             geo_data['fid'].append(idx_to_fid[v])
-            geo_data['og_coord'].append(coords_dict[k]['tuple'])
+            geo_data['og_coord'].append(
+                coords_dict[k]['tuple'])
         return pd.DataFrame(geo_data)
 
     def get_geodata_kdtree(self, coords):
@@ -109,9 +128,11 @@ class CoordinatesToCountries:
             list(self.df_cb_shifted['lat']),
             list(self.df_cb_shifted['lon'])))
         tree = KDTree(data, leafsize=30)
-        _, ii = tree.query(coords['coords'], k=1, workers=-1)
+        _, ii = tree.query(coords['coords'], k=1,
+            workers=-1)
         geo_data = {
-            'id': [], 'fid': [], 'country_code': [], 'og_coord': []}
+            'id': [], 'fid': [], 'country_code': [],
+            'og_coord': []}
         for idx, i in enumerate(ii):
             geo_data['id'].append(coords['id'][idx])
             fid = self.df_cb_shifted.iloc[[
@@ -120,7 +141,8 @@ class CoordinatesToCountries:
             cc = self.df_cb_shifted.iloc[[
                 i]].country_code.values[0]
             geo_data['country_code'].append(cc)
-            geo_data['og_coord'].append(coords['coords'][idx])
+            geo_data['og_coord'].append(
+                coords['coords'][idx])
         return pd.DataFrame(geo_data)
         
     def get_closest_admin(self, df_geo_data):
